35c85e225b6e5da4ce65f60fc1b1deeff1d4076b
pkoj0328@gmail.com <pkoj0328@gmail.com>
2026-08-27 21:31:17 +0900

    소개 메서드 추가

--- 260827/260827/Program.cs
+++ 260827/260827/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 
-public class Program
-{
-    public static void Main()
-    {
-        Console.WriteLine("=== 8조 팀원 소개 ===");
-        // 여기에 팀원 소개 호출을 한 줄씩 추가합니다.
+public class Program  
+{  
+    public static void Main()  
+    {  
+        Console.WriteLine("=== 8조 팀원 소개 ===");  
+        // 여기에 팀원 소개 호출을 한 줄씩 추가합니다
 
-        Console.WriteLine("=== 소개 끝 ===");
+        Console.WriteLine("=== 소개 끝 ===");  
     }
 
-    // 여기에 팀원 소개 메서드를 하나씩 추가합니다.
+    public static void IntroSample()  
+    {  
+        Console.WriteLine("mark / 8조 버전관리 과제");  
+    }
+
 }
