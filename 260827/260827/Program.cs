bc4930d4ae4f825b6294ed3403c87436db518e9e
Ash <lemoncube12@gmail.com>
2026-08-27 21:38:13 +0900

    [Feat] 팀원 소개 메서드 Jatdummy 추가 수정 마지막
    Closes #1

--- 260827/260827/Program.cs
+++ 260827/260827/Program.cs
@@ -5,11 +5,13 @@ public class Program
     public static void Main()
     {
         Console.WriteLine("=== 8조 팀원 소개 ===");
+        // 여기에 팀원 소개 호출을 한 줄씩 추가합니다
         Imjatdummy();
 
         Console.WriteLine("=== 소개 끝 ===");
     }
-
+    
+    // 여기에 팀원 소개 메서드를 하나씩 추가합니다  
     public static void Imjatdummy()
     {
         Console.WriteLine("정승원 / 라멘을 좋아합니다");
